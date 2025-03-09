9a2c57ac5c59e8559610d498ad51ad6da2db2472
Zneo111 <tevin.rutere@student.moringaschool.com>
2025-03-09 14:37:41 +0300

    Adds index.js and .results.json

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const companyName = 'Scuber'
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
+
+describe('index.js', function() {
+
+});
+
+it('is set as Scuber', function () {
+    // tests are here
+});
+
+it('is set as Scuber', function(){
+    expect(companyName).to.equal('Scuber');
+});
+
